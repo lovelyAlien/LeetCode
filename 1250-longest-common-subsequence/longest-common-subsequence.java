f2dd1d57545d74e9eccbcb03b72285f8e864896d
Jaeseung Choun <54395509+lovelyAlien@users.noreply.github.com>
2024-11-16 17:47:51 +0900

    Update longest-common-subsequence.java

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,19 +1,15 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        //난 됐다. 오늘의 집 개발자다. 그에 따른 행동을 하자. 
-
-        // text.charAt(i)
-        // 난 됐다. 오늘의 집 개발자다. 그에 따른 행동을 하자.
-
         // 최장 공통 부분 문자열을 찾아야 한다.
         // "abcde", "ace"
+        
+        int m = text1.length();
+        int n = text2.length();
+        // dp[i][j]는 text1의 1번째 문자부터 i번째 문자까지와 text2의 1번째 문자부터 j번째 문자까지에서의 최장 공통 부분 수열의 길이
+        int[][] dp= new int[m+1][n+1];
 
-        // dp[i][j]: 1번째 부터 i번째 까지 문자열과 1번째 부터 j번째 까지 문자열 최장 공통 부분 문자열 길이
-
-        int[][] dp= new int[text1.length()+1][text2.length()+1];
-
-        for(int i =1;i<=text1.length();i++)
-            for(int j =1;j<=text2.length();j++) {
+        for(int i =1;i<=m;i++)
+            for(int j =1;j<=n;j++) {
                 if(text1.charAt(i-1) == text2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
@@ -22,8 +18,6 @@ class Solution {
                 }
             }
 
-        return dp[text1.length()][text2.length()];
-
-        
+        return dp[m][n];        
     }
 }
